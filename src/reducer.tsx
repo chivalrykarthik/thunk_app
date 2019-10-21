b1ed714fd40947a9d74ae5897c61bd88eae7bce3
chivalrykarthik <chivalrykarthik@gmail.com>
2019-10-21 14:36:39 +0530

    Added new case for check box

--- src/reducer.tsx
+++ src/reducer.tsx
@@ -1,28 +1,37 @@
-//export const stateSelector = (state: any) => ({ ...state });
+export const stateSelector = (state:any) =>({...state});
 
-let initialState = { a: 1, status: "COMPLETED", title: "", body: "" };
-interface Action {
-	type: string,
-	data?: any
+let initialState = {a:1,status:"COMPLETED",title:"",body:"",userList:[]};
+interface Action{
+	type:string,
+	data?:any
 }
-const reducer = (state = initialState, action: Action) => {
+const reducer = (state=initialState,action:Action)=>{
 	let finalState;
-	switch (action.type) {
+	switch(action.type){
 		case "ADDUSER":
-			finalState = { ...state, ...action.data };
-			break;
+			finalState = {...state,...action.data};
+		break;
+		case "SHOWUSER":		
+			if(action.data instanceof Array)
+				finalState = {...state,userList:action.data};
+			else
+				finalState = {...state,userList:[action.data]};
+		break;
+		case "SUB":
+			finalState = {...state,a:state.a-1};
+		break;
 		case "DONE":
-			finalState = { ...state, status: "COMPLETED" };
-			break;
+			finalState = {...state,status:"COMPLETED"};
+		break;
 		case "INPROGRESS":
-			finalState = { ...state, status: "LOADING..." };
-			break;
+			finalState = {...state,status:"LOADING..."};
+		break;
 		case "ERROR":
-			finalState = { ...state, status: "No Valid response" };
-			break;
+			finalState = {...state,status:"No negative values"};
+		break;
 		default:
 			finalState = state;
-			break;
+		break;
 	}
 	return finalState;
 }
