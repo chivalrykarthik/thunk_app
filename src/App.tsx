25a38f74bcc6ec92c5ef5a2ed65fe5efb678edff
chivalrykarthik <chivalrykarthik@gmail.com>
2019-10-21 14:35:57 +0530

    Added check box in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import action from './action';
-//import { stateSelector } from './reducer';
+import {stateSelector} from './reducer';
 import User from './User';
-function App(props: any) {
-	return (
-		<div className="App">
-			Title: {props.state.title}
-			<br />
-			Body: {props.state.body}
-			<br />
-			status: {props.state.status}
-			<hr />
-			<User />
-		</div>
-	);
+import Checkbox from './CheckBox';
+function App(props:any) {
+  return (
+    <div className="App">
+	Title:{props.state.title} Body:{props.state.body}
+	<br />
+	status:{props.state.status}
+	<br />
+	
+	<User />
+	<br />
+	<Checkbox />
+    </div>
+  );
 }
 
-const mapStateToProps = (state: any) => {
-	return {
-		state: state
+const mapStateToProps = (state:any)=>{
+	return{
+		state:stateSelector(state)
 	}
 };
 
-const mapDispatchToProps = (dispatch: any) => {
+const mapDispatchToProps = (dispatch:any)=>{
 	return {
-		add: (state: any) => dispatch(action.add(state))
+		add:(state:any)=>dispatch(action.add(state)),
+		sub:()=>dispatch(action.sub())
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
